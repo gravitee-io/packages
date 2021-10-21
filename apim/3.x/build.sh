3d8620c53aa8e17dbabf130589a5f29a1853d996
Florent CHAMFROY <florent.chamfroy@graviteesource.com>
2021-10-21 22:31:38 +0200

    feat: adapt folder name
    portal-webui is now in monorepo
    
    gravitee-io/issues#6146

--- apim/3.x/build.sh
+++ apim/3.x/build.sh
@@ -150,8 +150,8 @@ build_portal_ui() {
 	rm -fr build/skel/
 
 	mkdir -p ${TEMPLATE_DIR}/opt/graviteeio/apim
-        cp -fr .staging/graviteeio-full-${VERSION}/graviteeio-portal-ui-${VERSION} ${TEMPLATE_DIR}/opt/graviteeio/apim
-	ln -sf ${TEMPLATE_DIR}/opt/graviteeio/apim/graviteeio-portal-ui-${VERSION} ${TEMPLATE_DIR}/opt/graviteeio/apim/portal-ui
+        cp -fr .staging/graviteeio-full-${VERSION}/graviteeio-apim-portal-ui-${VERSION} ${TEMPLATE_DIR}/opt/graviteeio/apim
+	ln -sf ${TEMPLATE_DIR}/opt/graviteeio/apim/graviteeio-apim-portal-ui-${VERSION} ${TEMPLATE_DIR}/opt/graviteeio/apim/portal-ui
 
 	mkdir -p ${TEMPLATE_DIR}/etc/nginx/conf.d/
 	cp build/files/graviteeio-apim-portal-ui.conf ${TEMPLATE_DIR}/etc/nginx/conf.d/
@@ -175,7 +175,7 @@ build_portal_ui() {
                 --url "${URL}" \
                 --description  "${DESC}: Portal UI" \
                 --depends nginx \
-		--config-files "${TEMPLATE_DIR}/opt/graviteeio/apim/graviteeio-portal-ui-${VERSION}/assets/" \
+		--config-files "${TEMPLATE_DIR}/opt/graviteeio/apim/graviteeio-apim-portal-ui-${VERSION}/assets/" \
                 --verbose \
                 -n ${PKGNAME}-portal-ui-3x
 }
